420e947a3021472f108901166d2e8769f8b9fa39
Furk <>
2022-07-11 14:53:26 +0300

    search viewmodel lifecycle

--- features/artistsearch/src/main/java/com/vfurkana/artistsearch/viewmodel/ArtistSearchViewModel.kt
+++ features/artistsearch/src/main/java/com/vfurkana/artistsearch/viewmodel/ArtistSearchViewModel.kt
@@ -18,17 +18,20 @@ class ArtistSearchViewModel @Inject constructor(val useCase: ArtistSearchUseCase
     private val queryFlow = MutableSharedFlow<String?>()
     val searchResults: Flow<ViewState<PagingData<Artist>>> =
         queryFlow
+            .distinctUntilChanged()
             .debounce(500)
             .filterNot { it.isNullOrEmpty() }
-            .flatMapLatest {
+            .transform {
                 useCase.searchArtistsPaged(it)
                     .cachedIn(viewModelScope)
                     .map<PagingData<Artist>, ViewState<PagingData<Artist>>> { ViewState.Success(it) }
                     .onStart { emit(ViewState.Progress) }
                     .onEmpty { emit(ViewState.Empty()) }
                     .catch { emit(ViewState.Error(it)) }
+                    .collect(this)
             }
             .onStart { emit(ViewState.Initial()) }
+            .shareIn(viewModelScope, SharingStarted.Eagerly)
 
     fun onSearchInput(input: String?) {
         viewModelScope.launch { queryFlow.emit(input) }
